2f4bf38232f250e8114f3898761e390162546d4e
Maryam-Saba <105993857+Maryam-Saba@users.noreply.github.com>
2022-08-14 03:39:14 +0500

    Add files via upload

---
+++ Multi.java
@@ -0,0 +1,17 @@
+public class Multi
+{
+public static void main(String args[])
+{
+int[][] x= new int[4][4];
+
+x[0][0]=5;
+x[0][1]=6;
+x[1][0]=7;
+x[1][1]=8;
+		
+		System.out.println(x[0][0] + x[0][1]);
+
+		System.out.println(x[1][0] + x[1][1]);
+
+	}
+}
